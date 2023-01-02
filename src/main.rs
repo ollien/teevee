eeeef56755dab10508d5b39f6eb88f3f4cee0a1d
Nick Krichevsky <nick@ollien.com>
2023-01-02 18:01:51 -0500

    Add config file flag

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 #![warn(clippy::all, clippy::pedantic)]
+use clap::Parser;
 use log::LevelFilter;
 use simplelog::{ColorChoice, Config as SimpleLogConfig, TermLogger, TerminalMode};
 use std::fs::File;
 use teevee::{stream_videos, Config};
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, default_value = "config.yml")]
+    config_file: String,
+}
+
 fn main() {
-    let config_reader = File::open("config.yml").expect("failed to open config file");
+    let args = Args::parse();
+
+    let config_reader = File::open(args.config_file).expect("failed to open config file");
     let config =
         serde_yaml::from_reader::<_, Config>(config_reader).expect("failed to parse config");
 
